eb387102a0e9c8a6157761a056ed30be00f3683c
linyyyang <linyyyang@tencent.com>
2020-12-16 20:26:36 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # koatty_static
 Static server Middleware for Koatty.
+
+# 安装
+-----
+
+```
+npm i koatty_static
+```
+
+# 使用
+-----
+
+Koatty集成了StaticMiddleware中间件, 但是默认并未开启, 需要修改中间配置开启
+
+3、修改项目中间件配置文件 config/middleware.ts:
+
+```
+config: { //中间件配置
+    ...,
+    // StaticMiddleware: false 如果配置了Nginx代理,请开启此行并注释掉下面配置
+    StaticMiddleware: {
+        dir: '/static', // resource path
+        prefix: '', // the url prefix you wish to add, default to ''
+        alias: {}, // object map of aliases. See below
+        gzip: true, // when request's accept-encoding include gzip, files will compressed by gzip.
+        usePrecompiledGzip: false, // try use gzip files, loaded from disk, like nginx gzip_static
+        buffer: false, // store the files in memory instead of streaming from the filesystem on each request
+        filter: [], // (function | array) - filter files at init dir, for example - skip non build (source) files. If array set - allow only listed files
+        maxAge: 3600 * 24 * 7, // cache control max age for the files, 0 by default.
+        preload: false, // caches the assets on initialization or not, default to true. always work together with options.dynamic.
+        cache: false // dynamic load file which not cached on initialization.
+    }
+}
+```
---
+++ src/index.ts
@@ -0,0 +1,73 @@
+/*
+ * @Author: richen
+ * @Date: 2020-12-16 19:51:00
+ * @LastEditors: linyyyang<linyyyang@tencent.com>
+ * @LastEditTime: 2020-12-16 20:09:01
+ * @License: BSD (3-Clause)
+ * @Copyright (c) - <richenlin(at)gmail.com>
+ */
+import path from "path";
+import * as lib from "koatty_lib";
+import LRU from "lru-cache";
+import staticCache from "koa-static-cache";
+
+/** @type {*} */
+const files: any = new LRU({ max: 1000 });
+
+/**
+ *
+ *
+ * @interface OptionsInterface
+ */
+interface OptionsInterface {
+    dir: string;
+    prefix: string;
+    alias: any;
+    gzip: boolean;
+    usePrecompiledGzip: boolean;
+    buffer: boolean;
+    filter: any[];
+    maxAge: number;
+    preload: boolean;
+    cache: boolean;
+}
+
+/**
+ * default options
+ */
+const defaultOptions: OptionsInterface = {
+    dir: '/static', // resource path
+    prefix: '', // the url prefix you wish to add, default to ''
+    alias: {}, // object map of aliases. See below
+    gzip: true, // when request's accept-encoding include gzip, files will compressed by gzip.
+    usePrecompiledGzip: false, // try use gzip files, loaded from disk, like nginx gzip_static
+    buffer: false, // store the files in memory instead of streaming from the filesystem on each request
+    filter: [], // (function | array) - filter files at init dir, for example - skip non build (source) files. If array set - allow only listed files
+    maxAge: 3600 * 24 * 7, // cache control max age for the files, 0 by default.
+    preload: false, // caches the assets on initialization or not, default to true. always work together with options.dynamic.
+    cache: false // dynamic load file which not cached on initialization.
+};
+
+export function Static(options: any, app: any) {
+    options = options ? lib.extend(defaultOptions, options, true) : defaultOptions;
+    // static path
+    if (options.dir === '/' || options.dir === '') {
+        options.dir = '/static';
+    }
+    /*eslint-disable consistent-return */
+    return staticCache(
+        path.join(app.rootPath || process.env.ROOT_PATH || '', options.dir),
+        {
+            prefix: options.prefix,
+            alias: options.alias,
+            gzip: options.gzip,
+            usePrecompiledGzip: options.usePrecompiledGzip,
+            buffer: options.buffer,
+            filter: options.filter,
+            dynamic: options.cache,
+            maxAge: options.maxAge,
+            preload: options.preload,
+        },
+        files,
+    );
+}
